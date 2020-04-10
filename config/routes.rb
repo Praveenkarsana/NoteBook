5d8e33ef6ba3ff2844d63f69ef6d8be6ea6ba56c
Praveenkarsana <praveenkarsana17@gmial.com>
2020-04-11 00:58:30 +0530

    Changed root based on user signed_in or not

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  
-  root 'welcome#index'
-
   resources :notes
+
+  authenticated :user do
+  	root'notes#index', as: "authenticated_root"
+  end										#viwe for loged in user with all post if not it will just show welcome post
+
+  root 'welcome#index'				#non loged in user
+
+  
 end
